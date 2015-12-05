9e2db552f28c3150b47e36bbd393af224f3ef8b1
Dan Mercurio <dan.j.mercurio@gmail.com>
2015-12-05 00:25:26 -0800

    added warning about mysql2 gem version

--- db/schema.rb
+++ db/schema.rb
@@ -17,36 +17,13 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "asset_id"
     t.integer  "altitude"
     t.integer  "icon_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "aircrafts", :force => true do |t|
-    t.integer  "asset_id"
-    t.integer  "altitude"
-    t.integer  "icon_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "ais_ship_type_icons", :force => true do |t|
     t.integer "ship_type_code"
     t.integer "icon_id"
-  end
-
-  create_table "area_categories", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "description"
-  end
-
-  create_table "asset_details", :force => true do |t|
-    t.string   "name"
-    t.text     "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "asset_id"
   end
 
   create_table "asset_types", :force => true do |t|
@@ -62,8 +39,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "visibility_id"
     t.boolean  "is_active",           :default => true
     t.string   "common_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
   end
 
   create_table "client_share_privileges", :id => false, :force => true do |t|
@@ -83,14 +60,9 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string   "contact_email"
     t.text     "contact_notes"
     t.boolean  "active",        :default => true
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.string   "state"
-  end
-
-  create_table "clients_layers", :id => false, :force => true do |t|
-    t.integer "client_id"
-    t.integer "layer_id"
   end
 
   create_table "clients_layers_privileges", :id => false, :force => true do |t|
@@ -110,8 +82,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
   create_table "custom_fields", :force => true do |t|
     t.string   "name"
     t.text     "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "asset_id"
   end
 
@@ -124,19 +96,19 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.datetime "locked_at"
     t.datetime "failed_at"
     t.text     "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "device_types", :force => true do |t|
-    t.string "name",        :null => false
-    t.string "title"
+    t.string "name"
     t.text   "description"
+    t.string "title"
   end
 
   create_table "devices", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.integer  "client_id"
     t.integer  "device_type_id"
     t.string   "request_key"
@@ -145,9 +117,11 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.boolean  "is_active",      :default => true
   end
 
+  add_index "devices", ["serial_number"], :name => "index_devices_on_serial_number", :unique => true
+
   create_table "faria_feeds", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "fish", :force => true do |t|
@@ -163,19 +137,22 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string  "duties"
   end
 
-  create_table "fishing_area_points", :force => true do |t|
+  create_table "fishing_area_points", :id => false, :force => true do |t|
+    t.integer "id"
     t.integer "fishing_area_id"
     t.float   "lat"
     t.float   "lon"
   end
 
-  create_table "fishing_area_types", :force => true do |t|
+  create_table "fishing_area_types", :id => false, :force => true do |t|
+    t.integer  "id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "fishing_areas", :force => true do |t|
+  create_table "fishing_areas", :id => false, :force => true do |t|
+    t.integer  "id"
     t.string   "name"
     t.integer  "fishing_area_type_id"
     t.string   "line_type"
@@ -191,8 +168,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "fishing_trip_id"
     t.float    "amount"
     t.string   "type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "fishing_crews", :force => true do |t|
@@ -223,8 +200,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string   "confirmation"
     t.datetime "date_submitted"
     t.datetime "date_certified"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "fishing_vessels", :force => true do |t|
@@ -242,37 +219,38 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string   "vms_number"
     t.string   "vms_passcode"
     t.string   "vms_email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "grp_areas", :force => true do |t|
     t.integer  "grp_plan_id"
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "grp_boom_types", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "grp_booms", :force => true do |t|
     t.integer  "grp_id"
     t.integer  "grp_boom_type_id"
-    t.decimal  "start_lat",        :precision => 10, :scale => 0
-    t.decimal  "start_lon",        :precision => 10, :scale => 0
-    t.decimal  "end_lat",          :precision => 10, :scale => 0
-    t.decimal  "end_lon",          :precision => 10, :scale => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.decimal  "start_lat",        :precision => 9,  :scale => 7
+    t.decimal  "start_lon",        :precision => 10, :scale => 7
+    t.decimal  "end_lat",          :precision => 9,  :scale => 7
+    t.decimal  "end_lon",          :precision => 10, :scale => 7
+    t.datetime "created_at",                                      :null => false
+    t.datetime "updated_at",                                      :null => false
     t.string   "description"
   end
 
-  create_table "grp_plans", :force => true do |t|
+  create_table "grp_plans", :id => false, :force => true do |t|
+    t.integer  "id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -294,8 +272,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string   "status"
     t.float    "lat"
     t.float    "lon"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.integer  "state"
   end
 
@@ -308,8 +286,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
   create_table "images", :force => true do |t|
     t.integer  "asset_id"
     t.text     "caption"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "kmls", :force => true do |t|
@@ -319,29 +297,19 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "sort"
     t.integer  "active",         :null => false
     t.integer  "stored_file_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "layers", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "title"
     t.string   "icon"
     t.integer  "sort"
     t.string   "category"
-  end
-
-  create_table "layers_maps", :id => false, :force => true do |t|
-    t.integer "layer_id"
-    t.integer "map_id"
-  end
-
-  create_table "layers_users", :id => false, :force => true do |t|
-    t.integer "layer_id"
-    t.integer "user_id"
   end
 
   create_table "layers_users_privileges", :id => false, :force => true do |t|
@@ -354,8 +322,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "user_id"
     t.datetime "expires"
     t.boolean  "deactivated"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "logins", :force => true do |t|
@@ -363,32 +331,21 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.datetime "created_at"
   end
 
-  create_table "map_areas", :force => true do |t|
-    t.string   "name"
-    t.integer  "area_category_id"
-    t.string   "line_type"
-    t.float    "lat"
-    t.float    "lon"
-    t.string   "color"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "maps", :force => true do |t|
     t.integer  "user_id"
     t.integer  "zoom",       :default => 10
-    t.float    "lon"
+    t.float    "lon",        :default => -123.436
     t.float    "lat",        :default => 46.753
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "map_type",   :default => "Map",  :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.string   "map_type",   :default => "Map"
   end
 
   create_table "others", :force => true do |t|
     t.integer  "asset_id"
     t.integer  "icon_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "past_locations", :force => true do |t|
@@ -399,19 +356,11 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "client_id"
   end
 
-  create_table "points", :force => true do |t|
-    t.integer  "map_area_id"
-    t.float    "lat"
-    t.float    "lon"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "privileges", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "route_points", :force => true do |t|
@@ -422,8 +371,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
   end
 
   create_table "routes", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "asset_id"
     t.string   "name"
     t.text     "description"
@@ -433,8 +382,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "user_id"
     t.string   "ip_address"
     t.string   "path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "shared_assets", :force => true do |t|
@@ -451,15 +400,15 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
 
   create_table "ships", :force => true do |t|
     t.integer  "asset_id"
-    t.decimal  "dim_bow",       :precision => 10, :scale => 0
-    t.decimal  "dim_stern",     :precision => 10, :scale => 0
-    t.decimal  "dim_starboard", :precision => 10, :scale => 0
-    t.decimal  "dim_port",      :precision => 10, :scale => 0
-    t.decimal  "draught",       :precision => 10, :scale => 0
+    t.decimal  "dim_bow",       :precision => 5, :scale => 1
+    t.decimal  "dim_stern",     :precision => 5, :scale => 1
+    t.decimal  "dim_starboard", :precision => 5, :scale => 1
+    t.decimal  "dim_port",      :precision => 5, :scale => 1
+    t.decimal  "draught",       :precision => 5, :scale => 1
     t.string   "destination"
     t.datetime "eta"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.float    "cog"
     t.float    "speed"
     t.boolean  "at_port"
@@ -467,32 +416,53 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.integer  "icon_id"
   end
 
+  add_index "ships", ["asset_id"], :name => "unique_asset", :unique => true
+
   create_table "staging_area_asset_details", :force => true do |t|
     t.integer "staging_area_asset_id"
     t.string  "name"
     t.string  "value"
   end
 
-  create_table "staging_area_asset_types", :force => true do |t|
+  add_index "staging_area_asset_details", ["staging_area_asset_id"], :name => "index_staging_area_asset_details_on_staging_area_asset_id"
+
+  create_table "staging_area_asset_types", :id => false, :force => true do |t|
+    t.integer  "id"
     t.integer  "staging_area_company_id"
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
     t.integer  "access_id"
   end
 
-  create_table "staging_area_assets", :force => true do |t|
-    t.integer "staging_area_id"
-    t.integer "staging_area_asset_type_id"
-    t.text    "description"
-    t.string  "access_id"
-    t.string  "parent_access_id"
-    t.string  "image"
-  end
+  add_index "staging_area_asset_types", ["staging_area_company_id"], :name => "index_staging_area_asset_types_on_staging_area_company_id"
 
-  create_table "staging_area_companies", :force => true do |t|
+  create_table "staging_area_assets", :id => false, :force => true do |t|
+    t.integer  "id"
+    t.integer  "staging_area_id"
+    t.integer  "staging_area_asset_type_id"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "boom_length"
+    t.string   "recovery"
+    t.string   "liquid_storage"
+    t.string   "access_id"
+    t.string   "parent_access_id"
+    t.string   "requirements"
+    t.string   "serial_number"
+    t.string   "model_name"
+    t.string   "manufacture_year"
+    t.string   "image"
+    t.string   "specification"
+  end
+
+  add_index "staging_area_assets", ["staging_area_asset_type_id"], :name => "index_staging_area_assets_on_staging_area_asset_type_id"
+  add_index "staging_area_assets", ["staging_area_id"], :name => "index_staging_area_assets_on_staging_area_id"
+
+  create_table "staging_area_companies", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "title"
     t.integer  "layer_id"
   end
@@ -503,22 +473,30 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.string  "value"
   end
 
-  create_table "staging_areas", :force => true do |t|
-    t.string  "name",                                                   :default => "unknown", :null => false
-    t.integer "staging_area_company_id"
-    t.string  "contact"
-    t.string  "address"
-    t.string  "city"
-    t.string  "phone"
-    t.string  "fax"
-    t.string  "state"
-    t.string  "zip"
-    t.string  "email"
-    t.decimal "lat",                     :precision => 10, :scale => 7
-    t.decimal "lon",                     :precision => 10, :scale => 7
-    t.integer "access_id"
-    t.string  "icon"
+  add_index "staging_area_details", ["staging_area_id"], :name => "index_staging_area_details_on_staging_area_id"
+
+  create_table "staging_areas", :id => false, :force => true do |t|
+    t.integer  "id"
+    t.string   "name",                                                   :default => "unknown", :null => false
+    t.integer  "staging_area_company_id"
+    t.string   "contact"
+    t.string   "address"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "email"
+    t.decimal  "lat",                     :precision => 9,  :scale => 7
+    t.decimal  "lon",                     :precision => 10, :scale => 7
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "specifications"
+    t.integer  "access_id"
+    t.string   "icon"
   end
+
+  add_index "staging_areas", ["staging_area_company_id"], :name => "index_staging_areas_on_staging_area_company_id"
 
   create_table "stored_files", :force => true do |t|
     t.string  "description"
@@ -535,15 +513,15 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
   create_table "tags", :force => true do |t|
     t.integer  "asset_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "tethers", :force => true do |t|
     t.integer  "asset_id"
     t.integer  "device_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -558,8 +536,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
     t.boolean  "active",          :default => true
     t.boolean  "eula",            :default => false
     t.datetime "last_login"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "time_zone"
   end
 
@@ -569,8 +547,8 @@ ActiveRecord::Schema.define(:version => 20130123012818) do
   end
 
   create_table "watchdogs", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
