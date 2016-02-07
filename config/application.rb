a43070ffa3bd4ca81c1bb39f858976e5edf8bd24
Dan Mercurio <dan.j.mercurio@gmail.com>
2016-02-06 19:34:41 -0800

    Backburner worker now forks from process

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Ddmap
         config.retry_delay_proc    = lambda { |min_retry_delay, num_retries| min_retry_delay + (num_retries ** 3) }
         config.default_priority    = 65536
         config.respond_timeout     = 120
-        config.default_worker      = Backburner::Workers::Simple
+        config.default_worker      = Backburner::Workers::ThreadsOnFork
         config.logger              = Logger.new(STDOUT)
         config.primary_queue       = "backburner-jobs"
         config.priority_labels     = { :custom => 50, :useless => 1000 }
